cc7c1101d5d3344ee760e2183041589c6fc38375
JustHNR <maths.honore@gmail.com>
2024-01-09 17:50:31 +0100

    "affichage side bar"

--- include/graph.h
+++ include/graph.h
@@ -4,6 +4,7 @@
 #include <MLV/MLV_all.h>
 #include "grid.h"
 #include "monstre.h"
+#include "game.h"
 
 #define CELL_SIZE 25 
 #define WIDTH 28     
@@ -16,5 +17,6 @@ void draw_cell(int x, int y, MLV_Color color);
 void draw_grid_with_path(int grid[HEIGHT][WIDTH], Point *path, int pathSize);
 void draw_start_and_finish(Point start, Point finish);
 void drawMonsters(Monster monsters[], int count);
-
+void drawAll(Game * , Monster * , int );
+void draw_side_information(Game *);
 #endif
