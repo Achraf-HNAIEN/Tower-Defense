e2558020958dda4f7b182fcd4ac62ebcc8b5f731
Achraf-HNAIEN <achraf.hnaien62@gmail.com>
2024-01-09 13:33:17 +0100

    monstre update

--- include/monstre.h
+++ include/monstre.h
@@ -5,7 +5,16 @@
 
 #define MONSTER_COUNT 30
 
-#define ELEMENTAL_TYPE_nb 3
+#define WAVE_INTERVAL 35  // Interval in seconds
+#define PROB_NORMAL 0.50  // Proba normal wave
+#define PROB_CROWD  0.20  // Proba crowd wave
+#define PROB_AGILE  0.20  // Proba agile wave
+#define PROB_BOSS   0.10  // Proba boss wave
+
+#define COUNT_NORMAL 12   // normal wave
+#define COUNT_CROWD  24   // crowd wave
+#define COUNT_AGILE  12   // agile wave
+#define COUNT_BOSS   2    // boss wave
 
 typedef enum {
     ELEMENT_NONE,
@@ -13,6 +22,13 @@ typedef enum {
     ELEMENT_DENDRO,
     ELEMENT_HYDRO
 } ElementalType;
+
+typedef enum {
+    NORMAL,
+    CROWD,
+    AGILE,
+    BOSS
+} MonsterType;
 
 typedef struct Monster {
     float hp;          
@@ -23,6 +39,7 @@ typedef struct Monster {
     int pathIndex;     
     int wave_number;   
     ElementalType elemental_type; 
+    MonsterType type;
     float elemental_residue;      
     float mana_reward;            
     float mana_penalty;           
@@ -36,5 +53,6 @@ void initializeNormalMonsters(Monster monsters[], int wave, Point path[]);
 void initializeCrowdMonsters(Monster monsters[], int wave, Point path[]);
 void initializeQuickMonsters(Monster monsters[], int wave, Point path[]);
 void initializeBosskMonsters(Monster monsters[], int wave, Point path[]);
+MonsterType selectWaveType(int waveNumber);
 void moveMonsters(Monster monsters[], int count, Point path[], int pathSize, float deltaTime);
 #endif
--- src/monstre.c
+++ src/monstre.c
@@ -1,5 +1,6 @@
 #include "monstre.h"
 #include <math.h>
+#include <stdlib.h>
 /*
 void initializeMonsters(Monster monsters[], int count, Point path[]) {
     const float spawnDelay = 60.0f;
@@ -110,5 +111,82 @@ void initializeBosskMonsters(Monster monsters[], int wave, Point path[]){
         monsters[i].y = path[0].y; 
         monsters[i].damage = 10; 
         monsters[i].pathIndex = -i * spawnDelay;
+    }
+}
+MonsterType selectWaveType(int waveNumber) {
+    float randomValue = (float)rand() / RAND_MAX;
+
+    // For first 5 waves, exclude the BOSS wave type
+    if (waveNumber <= 5) {
+        if (randomValue < PROB_NORMAL) {
+            return NORMAL;
+        } else if (randomValue < PROB_NORMAL + PROB_CROWD) {
+            return CROWD;
+        } else {
+            return AGILE;
+        }
+    } else {
+        // Include BOSS wave type for wave numbers above 5
+        if (randomValue < PROB_NORMAL) {
+            return NORMAL;
+        } else if (randomValue < PROB_NORMAL + PROB_CROWD) {
+            return CROWD;
+        } else if (randomValue < PROB_NORMAL + PROB_CROWD + PROB_AGILE) {
+            return AGILE;
+        } else {
+            return BOSS;
+        }
+    }
+}
+
+
+void initializeWave(Monster monsters[], int waveNumber, Point path[], int pathSize) {
+    MonsterType waveType = selectWaveType(waveNumber);
+    int count = 0;
+    float baseHP = 100.0f; // Base HP, can be adjusted
+
+    // Determine the number of monsters and other properties based on wave type
+    switch (waveType) {
+        case NORMAL:
+            count = COUNT_NORMAL;
+            break;
+        case CROWD:
+            count = COUNT_CROWD;
+            break;
+        case AGILE:
+            count = COUNT_AGILE;
+            break;
+        case BOSS:
+            count = COUNT_BOSS;
+            break;
+    }
+
+    // Initialize monsters for the selected wave
+    for (int i = 0; i < count; i++) {
+        monsters[i].type = waveType;
+        monsters[i].wave_number = waveNumber;
+        monsters[i].x = path[0].x;
+        monsters[i].y = path[0].y;
+        monsters[i].elemental_type = ELEMENT_NONE; // Default elemental type
+        monsters[i].pathIndex = -i * WAVE_INTERVAL / count;
+
+        // Set HP and speed based on monster type
+        if (waveType == BOSS) {
+            monsters[i].hp = 12 * baseHP * pow(1.2, waveNumber);
+            monsters[i].speed = 1.0f;
+        } else {
+            monsters[i].hp = baseHP * pow(1.2, waveNumber);
+            monsters[i].speed = (waveType == AGILE) ? 2.0f : 1.0f;
+        }
+
+        // Initialize other properties
+        monsters[i].max_hp = monsters[i].hp;
+        monsters[i].damage = 10.0f; // Default damage, adjust as needed
+        monsters[i].elemental_residue = 0.0f; // No elemental residue initially
+        monsters[i].mana_reward = monsters[i].hp * 0.1f * pow(1.3, /* current mana level */);
+        monsters[i].mana_penalty = monsters[i].hp * 0.15f * pow(1.3, /* current mana level */);
+        monsters[i].status_effects = 0; // No status effects initially
+        monsters[i].status_duration = 0.0f;
+        monsters[i].effect_intensity = 0.0f;
     }
 }
